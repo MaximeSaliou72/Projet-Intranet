601cf73b87504f368c4820dd8bfdae49a9fe49ad
Maxime Saliou <maximesaliou72@gmail.com>
2023-02-06 15:13:57 +0100

    login page

--- client/src/views/login.jsx
+++ client/src/views/login.jsx
@@ -1,4 +1,6 @@
+
 const Login = () => {
+    
     return (
         <main className="loginPage">
             <h1>Connexion</h1>
@@ -6,12 +8,12 @@ const Login = () => {
             <form className="loginForm">
                 <div>
                     <label>Email </label>
-                    <input placeholder="ex: owen.lopez@exemple.com"></input>
+                    <input placeholder="ex: owen.lopez@exemple.com" name='email' type="email"></input>
                 </div>
      
                 <div>
                     <label>Mot de passe </label>
-                    <input text='password' placeholder="Password"></input>
+                    <input name="password" type="password" placeholder="Password"></input>
                 </div>
                 <button className="button">Connexion</button>
             </form>
