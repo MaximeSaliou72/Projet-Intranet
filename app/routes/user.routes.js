cc514fc0712b79c7578d89e41b7ecfe3bd5d0fa4
vincent <vincent.lol.vs@gmail.com>
2023-02-08 09:49:15 +0100

    jwtOk

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,11 +1,13 @@
 const controller = require("../controllers/user.controller");
-// const uploadController = require("../controllers/upload.controller");
-// const multer = require("multer");
-// const upload = multer();
+const middleware = require("../middleware/authmiddleware.js");
+const uploadController = require("../controllers/upload.controller");
+const multer = require("multer");
+const upload = multer();
 
 module.exports = function (app) {
-  app.get("/app/user/:id", controller.userInfo);
-  app.get("/app/all", controller.getAllUsers);
-//   pp.post("app/upload", upload.single("file"), uploadController.uploadProfil);
+  app.get("/app/user/:id", middleware.headersTest,controller.userInfo);
+  app.get("/app/all",middleware.headersTest, controller.getAllUsers);
+  app.post("app/upload", upload.single("file"), uploadController.uploadProfil);
+
   app.put("/app/user/:id", controller.updateUser);
 };
